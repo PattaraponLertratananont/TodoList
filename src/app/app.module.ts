6b8cf94e5d620b3310107d5a7d502d45f68740d3
Pattarapon Lertratananont <pattaraponler@gmail.com>
2018-12-27 16:59:24 +0700

    ANGULAR{dialog}:add dialog to app.component.ts || click button add show dialog empty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
 import { DialogComponent, DialogContent } from './dialog/dialog.component';
 
@@ -15,11 +18,16 @@ import { DialogComponent, DialogContent } from './dialog/dialog.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatDialogModule
+    MatDialogModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatButtonModule,    
   ],
   entryComponents:[
-    DialogComponent,
-    DialogContent
+    [DialogComponent,DialogContent]
+  ],
+  exports:[
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
