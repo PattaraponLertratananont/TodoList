21126497f1d66ebaf4f004bbcc5a8e4954535aba
tammarut <tammarut>
2018-12-28 16:23:40 +0700

    create 2 struct 1)TodoList: keep Name,Duedate. 2)ListDb: database slice Lists[]TodoList, Type and then create function PostData.

--- API/main.go
+++ API/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
@@ -11,13 +14,22 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
+type TodoList struct {
+	Name    string `json:"name"`
+	Duedate string `json:"duedate"`
+}
+type ListDb struct {
+	Lists []TodoList
+	Type  string
+}
+
 // Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
 
+// Echo instance
 func main() {
-	// Echo instance
 	e := echo.New()
 
 	// Middleware
@@ -32,6 +44,8 @@ func main() {
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
+//█████████████████████████████████████████████████████████████████
+//Method
 func Getdata(c echo.Context) error {
 	// Open our jsonFile
 	jsonFile, err := os.Open("messege.json")
@@ -49,4 +63,20 @@ func Getdata(c echo.Context) error {
 	json.Unmarshal([]byte(byteValue), &result)
 
 	return c.JSON(http.StatusOK, result["MSG"])
+}
+
+func PostData(c echo.Context) error {
+	todoLists := []TodoList{}
+	db := ListDb{Lists: todoLists, Type: "Simple"}
+
+	buf := new(bytes.Buffer)
+	encoder := json.NewEncoder(buf)
+	encoder.Encode(db)
+
+	file, err := os.Create("todolist.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+	io.Copy(file, buf)
 }
