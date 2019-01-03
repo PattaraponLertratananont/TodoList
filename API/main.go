be42f01de67a462eec32ded1b172caded12a6017
tammarut <tammarut>
2019-01-03 11:23:54 +0700

    API: Getdata read code and comment each lines what they do, PostTest rename f =>file.

--- API/main.go
+++ API/main.go
@@ -54,40 +54,47 @@ func main() {
 
 //Method
 func PostTest(c echo.Context) error {
-	//write
-	message := `{"message": "Helsdsdlo","duedate": "01/01/19"}` + "\n"
-	// If the file doesn't exist, create it, or append to the file
-	f, err := os.OpenFile("message.txt", os.O_APPEND|os.O_WRONLY, 0644)
+	//! Write file
+	message := `{"message": "Go home","duedate": "05/01/19"}` + "\n"
+
+	//! If the file doesn't exist, create it, or append to the file
+	file, err := os.OpenFile("message.txt", os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if _, err := f.Write([]byte(message)); err != nil {
+	if _, err := file.Write([]byte(message)); err != nil {
 		log.Fatal(err)
 	}
-	if err := f.Close(); err != nil {
+	if err := file.Close(); err != nil {
 		log.Fatal(err)
 	}
 	return c.String(http.StatusOK, "We got your list.")
 }
 
 func Getdata(c echo.Context) error {
+	//! Openfile message.txt ที่เก็บข้อมูลต่างๆ ไว้
 	fileHandle, _ := os.Open("message.txt")
 	defer fileHandle.Close()
+
+	//! Scanของข้างใน
 	fileScanner := bufio.NewScanner(fileHandle)
 	var data []string
+	//! จับของยัดใส่slice data string
 	for fileScanner.Scan() {
 		txt := fileScanner.Text()
 		data = append(data, txt)
 		fmt.Println("data : " + txt)
 
 	}
-
-	bdata := strings.Join(data, ",")
+	//! Unfurl to pure string from slice []string
+	bytedata := strings.Join(data, ",")
 	//datasJson, _ := json.Marshal(data)
-	fmt.Println("data 2: " + bdata)
+	fmt.Println("data 2: " + bytedata)
 
+	//! Put result to TodoList struct
 	result := []TodoList{}
-	json.Unmarshal([]byte("["+bdata+"]"), &result)
+	//* "[" and "]" are making data to json.format
+	json.Unmarshal([]byte("["+bytedata+"]"), &result)
 
 	return c.JSON(http.StatusOK, result)
 }
